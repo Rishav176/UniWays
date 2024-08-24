49e84e87708c9cca6efbd063c54cfe545f2bb561
Rishav Kumar <rishavkr1766@gmail.com>
2024-08-24 17:20:03 -0600

    add route mapping functionality

--- src/app/d/[day]/paths/page.tsx
+++ src/app/d/[day]/paths/page.tsx
@@ -8,14 +8,14 @@ import {
 } from "@vis.gl/react-google-maps";
 
 export default function Paths() {
-  const position = { lat: 37.7749, lng: -122.4194 };
+  const position = { lat: 53.5232, lng: -113.5263 };
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}>
         <Map
           center={position}
-          zoom={9}
+          zoom={14}
           //mapId={process.env.NEXT_PUBLIC_MAP_ID}
           fullscreenControl={false}
         >
@@ -29,6 +29,36 @@ export default function Paths() {
 function Directions() {
   const map = useMap();
   const routesLibrary = useMapsLibrary("routes");
+  const [directionsService, setDirectionsService] =
+    useState<google.maps.DirectionsService>();
+
+  const [directionsRenderer, setDirectionsRenderer] =
+    useState<google.maps.DirectionsRenderer>();
+
+  const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([]);
+
+  useEffect(() => {
+    if (!routesLibrary || !map) return;
+    setDirectionsService(new routesLibrary.DirectionsService());
+    setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }));
+  }, [routesLibrary, map]);
+
+  useEffect(() => {
+    if (!directionsService || !directionsRenderer) return;
+    directionsService
+      .route({
+        origin: "DICE Ualberta",
+        destination: "Tory Building",
+        travelMode: google.maps.TravelMode.WALKING,
+        provideRouteAlternatives: true,
+      })
+      .then((response) => {
+        directionsRenderer.setDirections(response);
+        setRoutes(response.routes);
+      });
+  }, [directionsService, directionsRenderer]);
+
+  console.log(routes);
 
   return null;
 }
