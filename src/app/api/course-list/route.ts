1c02b9bc291fa140a5dc8f97ed8109db20fe77e7
Rishav Kumar <rishavkr1766@gmail.com>
2024-08-25 14:05:43 -0600

    chore: Refactor API routes for course list and course creation

--- src/app/api/course-list/route.ts
+++ src/app/api/course-list/route.ts
@@ -3,33 +3,33 @@ import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
-	try {
-		const { userId } = auth();
+  try {
+    const { userId } = auth();
 
-		if (!userId) {
-			return new NextResponse("User Not Authenticated", { status: 401 });
-		}
+    if (!userId) {
+      return new NextResponse("User Not Authenticated", { status: 401 });
+    }
 
-		const url = new URL(req.url);
-		const day = url.searchParams.get("day");
+    const url = new URL(req.url);
+    const day = url.searchParams.get("day");
 
-		if (!day) {
-			return new NextResponse("Invalid day parameter", { status: 400 });
-		}
+    if (!day) {
+      return new NextResponse("Invalid day parameter", { status: 400 });
+    }
 
-		const courseList = await db.course.findMany({
-			where: {
-				userId: userId,
-				day: day || undefined,
-			},
-			orderBy: {
-				startTime: "asc",
-			},
-		});
-
-		return NextResponse.json(courseList, { status: 200 });
-	} catch (error) {
-		console.error("GET, COURSE LIST ERROR:", error);
-		return new NextResponse("GET, COURSE LIST ERROR", { status: 500 });
-	}
+    const courseList = await db.course.findMany({
+      where: {
+        userId: userId,
+        day: day || undefined,
+      },
+      orderBy: {
+        startTime: "asc",
+      },
+    });
+    console.log("courseList::: ", courseList);
+    return NextResponse.json(courseList, { status: 200 });
+  } catch (error) {
+    console.error("GET, COURSE LIST ERROR:", error);
+    return new NextResponse("GET, COURSE LIST ERROR", { status: 500 });
+  }
 }
--- src/app/api/courses/route.ts
+++ src/app/api/courses/route.ts
@@ -3,36 +3,40 @@ import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-	try {
-		const { userId } = auth();
+  try {
+    const { userId } = auth();
 
-		if (!userId) {
-			return new NextResponse("User Not Authenticated", { status: 401 });
-		}
+    if (!userId) {
+      return new NextResponse("User Not Authenticated", { status: 401 });
+    }
 
-		const { courses, day } = await req.json();
+    const { courses, day } = await req.json();
 
-		console.log("courses::: ", courses);
+    console.log("courses::: ", courses);
 
-		// Validate the incoming data
-		if (!courses || !day) {
-			return new NextResponse("Invalid data", { status: 400 });
-		}
+    // Validate the incoming data
+    if (!courses || !day) {
+      return new NextResponse("Invalid data", { status: 400 });
+    }
 
-		const createNewCourse = await db.course.create({
-			data: {
-				name: courses[0].name,
-				startTime: courses[0].startTime,
-				endTime: courses[0].endTime,
-				location: courses[0].location,
-				day: day,
-				userId: userId,
-			},
-		});
+    const createNewCourse = await Promise.all(
+      courses.map((course: any) =>
+        db.course.create({
+          data: {
+            name: course.name,
+            startTime: course.startTime,
+            endTime: course.endTime,
+            location: course.location,
+            day: day,
+            userId: userId,
+          },
+        })
+      )
+    );
 
-		return NextResponse.json(createNewCourse, { status: 200 });
-	} catch (error) {
-		console.error("POST, NEW COURSE ERROR:", error);
-		return new NextResponse("POST, NEW COURSE ERROR", { status: 500 });
-	}
+    return NextResponse.json(createNewCourse, { status: 200 });
+  } catch (error) {
+    console.error("POST, NEW COURSE ERROR:", error);
+    return new NextResponse("POST, NEW COURSE ERROR", { status: 500 });
+  }
 }
